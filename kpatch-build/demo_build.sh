2c305229a03abef9d0f505bb7fa01d8077c8d05a
Chen Lei <losemyheaven@qq.com>
2020-06-15 17:05:39 +0800

    fix bug in demo_build.sh

--- kpatch-build/demo_build.sh
+++ kpatch-build/demo_build.sh
@@ -1,8 +1,8 @@
-GIT_PATH=/data/tkernel3-public-dev_tlinux3_longterm
-OUTPUT="/tmp/"
-KERN_TAG=4.14.105-19-0002_arm_support
-YOUR_PATCH_LIST=/data/test-kpatch/rename-ext4.diff
+GIT_PATH=/data/hotpatch/tkernel4 # your tkernel git dir
+CACHEDIR="/data/hotpatch-result" # your 
+KERN_TAG=5.4.32-1-tlinux4_hotpatch_v6
+YOUR_PATCH_LIST=/data/hotpatch/tkernel4/0001-xfs-hotpatch-test.patch
 
-./kpatch-build-tlinux -o "$OUTPUT" -g  "$GIT_PATH" -t "$KERN_TAG"  \
-	$YOUR_PATCH_LIST \
-	--skip-gcc-check --skip-cleanup
+CACHEDIR=$CACHEDIR ./kpatch-build-tlinux -g  "$GIT_PATH" -t "$KERN_TAG"  \
+		$YOUR_PATCH_LIST \
+		--skip-gcc-check --skip-cleanup
