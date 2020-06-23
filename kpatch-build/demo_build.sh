210b19ed8f7f9edbc765c8030ad095b3c60cfc45
lennychen <lennychen@tencent.com>
2020-06-23 21:42:30 +0800

    update demo_build.sh

--- kpatch-build/demo_build.sh
+++ kpatch-build/demo_build.sh
@@ -1,10 +1,10 @@
-GIT_PATH=/data/hotpatch/tkernel4
+GIT_PATH=/data/tkernel4
 CACHEDIR="/data/hotpatch-result"
 KERN_TAG=5.4.32-1-tlinux4_hotpatch_v6
-KERN_DEBUGINFO=/data/rpmbuild/RPMS/aarch64/kernel-debuginfo-5.4.32-1.tlinux4_hotpatch_v6.tl2.aarch64.rpm
-YOUR_PATCH_LIST=/data/hotpatch/tkernel4/0001-xfs-hotpatch-test.patch
+KERN_DEBUGINFO_DEVEL_ROOT=/data/rpm-test
+PATCH_LIST=/data/tkernel4/0001-xfs-hotpatch-test.patch
 
 CACHEDIR=$CACHEDIR ./kpatch-build-tlinux -g  "$GIT_PATH" -t "$KERN_TAG"  \
-	--skip-gcc-check --skip-cleanup --debuginfo $KERN_DEBUGINFO \
-	 $YOUR_PATCH_LIST \
+	--skip-gcc-check --skip-cleanup --debuginfo-devel-root $KERN_DEBUGINFO_DEVEL_ROOT \
+	 $PATCH_LIST
 
